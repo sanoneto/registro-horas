d7f95c48640198ccbe603da2e6100898262701a3
Abdenner <sanoneto@gmail.com>
2025-10-19 00:33:38 +0100

    adicionar paginação

--- horas_estagio/src/main/java/com/registo/horas_estagio/service/impl/RegisterHorasServiceImpl.java
+++ horas_estagio/src/main/java/com/registo/horas_estagio/service/impl/RegisterHorasServiceImpl.java
@@ -18,9 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;

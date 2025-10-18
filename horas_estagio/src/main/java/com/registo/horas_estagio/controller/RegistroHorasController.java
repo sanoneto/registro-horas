8266935fa71f9127f35f16aed37cdb31609f859b
Abdenner <sanoneto@gmail.com>
2025-10-18 20:36:46 +0100

    melhorias de codigo

--- horas_estagio/src/main/java/com/registo/horas_estagio/controller/RegistroHorasController.java
+++ horas_estagio/src/main/java/com/registo/horas_estagio/controller/RegistroHorasController.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/registros")
+@RequestMapping("/api/registos")
 @RequiredArgsConstructor
 public class RegistroHorasController {
 
